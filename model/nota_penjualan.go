71ce2765f32db2fdb61f977e21d04167de70d748
lutfihakim123 <lutfirahman400@gmail.com>
2022-10-20 13:43:41 +0700

    update type to sql.nullstring

--- model/nota_penjualan.go
+++ model/nota_penjualan.go
@@ -16,21 +16,21 @@ type Nota struct {
 }
 
 type ReportPenjualan struct {
-	Id            string       `json:"id"`
-	PelangganId   string       `json:"pelanggan_id" db:"pelanggan_id"`
-	KaryawanId    string       `json:"karyawan_id" db:"karyawan_id"`
-	MejaId        string       `json:"meja_id" db:"meja_id"`
-	MenuId        string       `json:"menu_id" db:"menu_id"`
-	NamaMenu      string       `json:"nama_menu" db:"nama_menu"`
-	NoMeja        string       `json:"no_meja" db:"no_meja"`
-	Harga         int          `json:"harga_menu" db:"harga_menu"`
-	Img           string       `json:"image_menu" db:"image_menu"`
-	WaktuPesan    time.Time    `json:"waktu_pesan" db:"waktu_pesan"`
-	NamaPelanggan string       `json:"nama_pelanggan" db:"nama_pelanggan"`
-	NamaKaryawan  string       `json:"nama_karyawan" db:"nama_karyawan"`
-	TempStock     int          `json:"temp_stock" db:"temp_stock"`
-	Kuantitas     int          `json:"kuantitas"`
-	Total         int          `json:"total" db:"total"`
-	CreatedAt     string       `db:"created_at"`
-	UpdatedAt     sql.NullTime `db:"updated_at"`
+	Id            string         `json:"id"`
+	PelangganId   string         `json:"pelanggan_id" db:"pelanggan_id"`
+	KaryawanId    string         `json:"karyawan_id" db:"karyawan_id"`
+	MejaId        string         `json:"meja_id" db:"meja_id"`
+	MenuId        sql.NullString `json:"menu_id" db:"menu_id"`
+	NamaMenu      sql.NullString `json:"nama_menu" db:"nama_menu"`
+	NoMeja        string         `json:"no_meja" db:"no_meja"`
+	Harga         sql.NullString `json:"harga_menu" db:"harga_menu"`
+	Img           sql.NullString `json:"image_menu" db:"image_menu"`
+	WaktuPesan    time.Time      `json:"waktu_pesan" db:"waktu_pesan"`
+	NamaPelanggan string         `json:"nama_pelanggan" db:"nama_pelanggan"`
+	NamaKaryawan  string         `json:"nama_karyawan" db:"nama_karyawan"`
+	TempStock     sql.NullInt64  `json:"temp_stock" db:"temp_stock"`
+	Kuantitas     sql.NullInt64  `json:"kuantitas"`
+	Total         sql.NullInt64  `json:"total" db:"total"`
+	CreatedAt     string         `db:"created_at"`
+	UpdatedAt     sql.NullTime   `db:"updated_at"`
 }
