812de43a75b1829e9f0752530d1db98fae323544
lutfihakim123 <lutfirahman400@gmail.com>
2022-10-20 10:26:29 +0700

    fix upload image

--- delivery/controller/menu_controller.go
+++ delivery/controller/menu_controller.go
@@ -74,7 +74,7 @@ func (mc *MenuController) UpdateMenu(ctx *gin.Context) {
 	} else {
 		responseUc, _ := mc.menuUseCase.GetMenuById(newMenu.Id)
 		nameFile := strings.Split(responseUc.Img, "/")
-		path := "assets/img/menu/" + nameFile[3]
+		path := "assets/img/menu/" + nameFile[5]
 		error := os.Remove(path)
 		if error != nil {
 			ctx.JSON(http.StatusBadRequest, gin.H{
@@ -130,7 +130,7 @@ func (mc *MenuController) DeleteMenu(ctx *gin.Context) {
 		})
 	} else {
 		nameFile := strings.Split(responseUc.Img, "/")
-		path := "assets/img/menu/" + nameFile[3]
+		path := "assets/img/menu/" + nameFile[5]
 		err := os.Remove(path)
 		ctx.JSON(http.StatusOK, gin.H{
 			"message": "ok",
