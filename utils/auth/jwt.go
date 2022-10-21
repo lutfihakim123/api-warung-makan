bf2d4b0bd91a87cbe50d2f9aadf92a40c488b061
lutfihakim123 <lutfirahman400@gmail.com>
2022-10-21 13:01:35 +0700

    remove  acces control origin

--- utils/auth/jwt.go
+++ utils/auth/jwt.go
@@ -32,14 +32,14 @@ func AuthTokenMiddleware() gin.HandlerFunc {
 				c.Abort()
 				return
 			}
-			c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
-			c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
-			c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
-			c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
-			if c.Request.Method == "OPTIONS" {
-				c.AbortWithStatus(204)
-				return
-			}
+			// c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+			// c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+			// c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+			// c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+			// if c.Request.Method == "OPTIONS" {
+			// 	c.AbortWithStatus(204)
+			// 	return
+			// }
 			token, err := Parsetoken(tokenString)
 			if err != nil {
 				c.JSON(401, gin.H{
